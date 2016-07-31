55a69ad5e8a472c0a98520792debcd0ed66f461e
Rob <irwin.shell@gmail.com>
2016-07-31 13:50:09 +1200

    upgrade react and react native

--- android/app/src/main/java/com/rncalc/MainActivity.java
+++ android/app/src/main/java/com/rncalc/MainActivity.java
@@ -1,15 +1,6 @@
 package com.rncalc;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -20,29 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RnCalc";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new ReactNativePushNotificationPackage(this),
-            new ReactNativeI18n(),
-            new RNDeviceInfo()
-        );
     }
 }
